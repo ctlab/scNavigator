339c95fe9af3d8284b8a62dcdf4c88388e31c615
Konstantin Zaitsev <zayats1812@gmail.com>
2020-02-05 11:40:58 +0000

    fixed main page

--- scn_js/src/components/MainPage.js
+++ scn_js/src/components/MainPage.js
@@ -15,8 +15,8 @@ class MainPage extends Component {
 
     render() {
         let mainPagePanes = [
-            { menuItem: 'Curated datasets', render: () => <Tab.Pane><CuratedDatasets /></Tab.Pane> },
             { menuItem: 'All scRNA-seq datasets', render: () => <Tab.Pane><PublicDatasets /></Tab.Pane> },
+            { menuItem: 'Curated datasets', render: () => <Tab.Pane><CuratedDatasets /></Tab.Pane> },
             { menuItem: 'Gene signature search', render: () => <Tab.Pane><GeneSignature /></Tab.Pane> },
         ];
         return (
@@ -30,20 +30,20 @@ class MainPage extends Component {
                             <p>
                                 Below we have a large collection of datasets and tools to play with:
                                 <ul>
-                                    <li> Collection of curated datasets. Curated dataset are those that we process by hand.
-                                        These include datasets from Human Cell Atlas (HCA), Tabula Muris and some of the datasets that
-                                        we generated in our lab.</li>
                                     <li> Large collection of automatically processed datasets.
                                         We processed almost every scRNA-seq dataset from GEO Omnibus database.
                                         We make it available for you in our browser.
                                     </li>
+									<li> Collection of curated datasets. Curated dataset are those that we process by hand.
+                                        These will include datasets from Human Cell Atlas (HCA), Tabula Muris and some of the datasets that
+                                        we generated in our lab.</li>
                                     <li>
                                         You can search for cell type specific gene signatures! When we processed all the public scRNA-seq datasets
                                         we also calculated all the markers of all the clusters in all these datasets. Just put a list of genes and we
                                         will tell you which cluster in which dataset it looks like.
                                     </li>
                                     <li>
-                                        If you were provided with secret dataset token, you can use it at the very bottom of the page
+                                        If you were provided with secret dataset token, you can use it at the very right of this page
                                     </li>
                                 </ul>
 
