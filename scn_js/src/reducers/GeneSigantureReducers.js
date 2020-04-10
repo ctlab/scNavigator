160e6bd0e975e2fdeb3efdc6c9520e845c77025c
askmebefore <zayats1812@mail.ru>
2020-04-10 17:23:59 +0300

    added fixes to convert any gene ids to gene symbol, so we can show the enrichment

--- scn_js/src/reducers/GeneSigantureReducers.js
+++ scn_js/src/reducers/GeneSigantureReducers.js
@@ -1,5 +1,5 @@
 import {GENE_SIGNATURE_INPUT_CHANGED, GENE_SIGNATURE_SUBMIT, GENE_SIGNATURE_RESULTS_LOADED} from "../actions";
-
+import _ from "lodash";
 
 export const geneSignature = (state, action) => {
     switch (action.type) {
@@ -10,6 +10,8 @@ export const geneSignature = (state, action) => {
             state.searchLoading = false;
             state.latestQuery = action.query;
             state.searchResults = action.results;
+            state.latestQuerySymbol = _.values(state.searchResults.result.geneConversionMapSymbol);
+            state.latestQuerySymbol = _.filter(state.latestQuerySymbol, (a) => !_.isNil(a));
             return state;
         case GENE_SIGNATURE_INPUT_CHANGED:
             state[action.name] = action.value;
