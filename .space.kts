00caf510016c83453ad28e08a201faa677180fde
askmebefore <zayats1812@mail.ru>
2020-08-12 17:30:23 +0300

    fixes

--- .space.kts
+++ .space.kts
@@ -1,4 +1,4 @@
-job("Test gradlew server") {
+job("build and test gradlew server") {
     gradlew("gradle:jdk8", "build") {
         env["MONGODB_HOST"] = "127.0.0.1:27017"
         env["MONGODB_DATABASE"] = "scn"
@@ -11,7 +11,7 @@ job("Test gradlew server") {
 
 job("Build frontend") {
     container("node:12-alpine") {
-        workDir = "./scn_js"
+        workDir = "/mnt/space/work/scn_js"
         shellScript {
             content = "npm install && npm run build"
         }
@@ -20,10 +20,13 @@ job("Build frontend") {
 
 job("Build and test python converter") {
     container("python:3.8-alpine") {
-        workDir = "./scn_h5_converter"
+        workDir = "/mnt/space/work/scn_h5_converter"
         shellScript {
             content = "pip install -r requirements.txt"
         }
+    }
+    container("python:3.8-alpine") {
+        workDir = "/mnt/space/work/scn_h5_converter"
         shellScript {
             content = "export PYTHONPATH=\$PYTHONPATH:`pwd` && pytest tests --typeguard-packages=converter"
         }
