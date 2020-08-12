dc784be6f392b89a3050be1283eff5c1312486b0
askmebefore <zayats1812@mail.ru>
2020-08-12 16:33:57 +0300

    adding meaningfull jobs

--- .space.kts
+++ .space.kts
@@ -1,11 +1,18 @@
-job("run shell script") {
-    container("ubuntu") {
+job("Test gradlew server") {
+    gradlew("gradle:jdk8", "build") {
+        env["MONGODB_HOST"]="127.0.0.1:27017"
+        env["MONGODB_DATABASE"]="scn"
+        env["MONGODB_COLLECTION"]="datasets"
+        env["PROD_PATH"]=""
+
+        service("mongo:latest")
+    }
+}
+
+job("Build frontend") {
+    container("node:12-alpine") {
         shellScript {
-            interpreter = "/bin/bash"
-            content = """
-                echo Hello
-                echo World!
-            """.trimIndent()
+            content = "cd scn_js && npm install && npm run build"
         }
     }
 }
