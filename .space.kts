14686e2ae59ff02ae497e76fde61d7c74410d2b3
askmebefore <zayats1812@mail.ru>
2020-08-12 17:07:32 +0300

    added python tests

--- .space.kts
+++ .space.kts
@@ -1,9 +1,9 @@
 job("Test gradlew server") {
     gradlew("gradle:jdk8", "build") {
-        env["MONGODB_HOST"]="127.0.0.1:27017"
-        env["MONGODB_DATABASE"]="scn"
-        env["MONGODB_COLLECTION"]="datasets"
-        env["PROD_PATH"]=""
+        env["MONGODB_HOST"] = "127.0.0.1:27017"
+        env["MONGODB_DATABASE"] = "scn"
+        env["MONGODB_COLLECTION"] = "datasets"
+        env["PROD_PATH"] = ""
 
         service("mongo:latest")
     }
@@ -11,8 +11,21 @@ job("Test gradlew server") {
 
 job("Build frontend") {
     container("node:12-alpine") {
+        workDir = "./scn_js"
         shellScript {
-            content = "cd scn_js && npm install && npm run build"
+            content = "npm install && npm run build"
+        }
+    }
+}
+
+job("Build and test python converter") {
+    container("python:3.8-alpine") {
+        workDir = "./scn_h5_converter"
+        shellScript {
+            content = "pip install -r requirements.txt"
+        }
+        shellScript {
+            content = "export PYTHONPATH=\$PYTHONPATH:`pwd` && pytest tests --typeguard-packages=converter"
         }
     }
 }
