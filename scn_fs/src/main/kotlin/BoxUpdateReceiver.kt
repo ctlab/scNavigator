b96e652202e901f3cea430b1cbae57c85109c4b2
Maksim Kleverov <klevermx@gmail.com>
2023-08-30 15:12:58 -0500

    print message

--- scn_fs/src/main/kotlin/BoxUpdateReceiver.kt
+++ scn_fs/src/main/kotlin/BoxUpdateReceiver.kt
@@ -157,6 +157,7 @@ suspend fun boxUpdateReceiver( // boxDir:Path,
                             } else {
                                 // Message is invalid, reject it
                                 Log.info("POST:  BAD box message")
+                                Log.info(body)
                                 call.respond(HttpStatusCode.BadRequest)
                             }
                     }
