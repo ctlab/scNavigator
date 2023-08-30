2d6de34a6c1b5d2ec5e8e9fc1ac5140cc1d63d78
Maksim Kleverov <klevermx@gmail.com>
2023-08-30 15:21:58 -0500

    try not concurent

--- scn_fs/src/main/kotlin/Watcher.kt
+++ scn_fs/src/main/kotlin/Watcher.kt
@@ -11,59 +11,60 @@ suspend fun recursiveFSWatcher(
     directoryToWatch: String,
     outChannel: Channel<Pair<Path, WatchEvent.Kind<Path>>>,
     pathKeys:ConcurrentHashMap<String, WatchKey>) {
-    val directoryFileObject = File(directoryToWatch)
-    val pathToWatch = directoryFileObject.toPath()
-
-        //TODO : use NIO
-    for (file in directoryFileObject.walk()) {
-        if (file.isDirectory) {
-            Log.info(file.absolutePath)
-            pathKeys[file.absolutePath] = file.toPath().register(watchService,
-                StandardWatchEventKinds.ENTRY_CREATE,
-                StandardWatchEventKinds.ENTRY_MODIFY,
-                StandardWatchEventKinds.ENTRY_DELETE
-            )
-        }
-    }
-
-    val pathKey = pathToWatch.register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
-        StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE)
-
-    while (true) {
-        val watchKey = watchService.take()
-
-        for (event in watchKey.pollEvents()) {
-
-            val fullPath = Paths.get(watchKey.watchable().toString(),
-                event.context().toString())
-            val fullFile = File(fullPath.toString())
-
-            if (fullFile.isDirectory) {
-                when (event.kind()) {
-                    StandardWatchEventKinds.ENTRY_CREATE -> {
-                        pathKeys[fullFile.absolutePath] = fullPath.register(watchService,
-                            StandardWatchEventKinds.ENTRY_CREATE,
-                            StandardWatchEventKinds.ENTRY_MODIFY,
-                            StandardWatchEventKinds.ENTRY_DELETE
-                        )
-                        Log.info("Now also watching directory ${fullFile.absolutePath}")
-                    }
-                    StandardWatchEventKinds.ENTRY_DELETE -> {
-                        pathKeys[fullFile.absolutePath]?.cancel()
-                        pathKeys.remove(fullFile.absolutePath)
-                        "Directory ${fullFile.absolutePath} is deleted, no longer watching it"
-                    }
-                }
-            } else {
-                // Log.info("SENDING EVENT TO FSReciever")
-                outChannel.send(Pair(fullPath, event.kind() as WatchEvent.Kind<Path>));
+        val directoryFileObject = File(directoryToWatch)
+        val pathToWatch = directoryFileObject.toPath()
+    
+        val pathKeys = hashMapOf<String, WatchKey>()
+    
+        for (file in directoryFileObject.walk()) {
+            if (file.isDirectory) {
+                Log.info(file.absolutePath)
+                pathKeys[file.absolutePath] = file.toPath().register(watchService,
+                    StandardWatchEventKinds.ENTRY_CREATE,
+                    StandardWatchEventKinds.ENTRY_MODIFY,
+                    StandardWatchEventKinds.ENTRY_DELETE
+                )
             }
         }
-
-        if (!watchKey.reset()) {
-            watchKey.cancel()
-//            break
+    
+        val pathKey = pathToWatch.register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
+            StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE)
+    
+        while (true) {
+            val watchKey = watchService.take()
+    
+            for (event in watchKey.pollEvents()) {
+    
+                val fullPath = Paths.get(watchKey.watchable().toString(),
+                    event.context().toString())
+                val fullFile = File(fullPath.toString())
+    
+                if (fullFile.isDirectory) {
+                    when (event.kind()) {
+                        StandardWatchEventKinds.ENTRY_CREATE -> {
+                            pathKeys[fullFile.absolutePath] = fullPath.register(watchService,
+                                StandardWatchEventKinds.ENTRY_CREATE,
+                                StandardWatchEventKinds.ENTRY_MODIFY,
+                                StandardWatchEventKinds.ENTRY_DELETE
+                            )
+                            Log.info("Now also watching directory ${fullFile.absolutePath}")
+                        }
+                        StandardWatchEventKinds.ENTRY_DELETE -> {
+                            pathKeys[fullFile.absolutePath]?.cancel()
+                            pathKeys.remove(fullFile.absolutePath)
+                            "Directory ${fullFile.absolutePath} is deleted, no longer watching it"
+                        }
+                    }
+                } else {
+                    // Log.info("SENDING EVENT TO FSReciever")
+                    outChannel.send(Pair(fullPath, event.kind() as WatchEvent.Kind<Path>));
+                }
+            }
+    
+            if (!watchKey.reset()) {
+                watchKey.cancel()
+    //            break
+            }
         }
-    }
-//    pathKey.cancel()
+    //    pathKey.cancel()
 }
